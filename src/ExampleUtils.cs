878937cb57608be49d7dcc0d248fe21cf4d572c4
Nelson Nobre <technobre@gmail.com>
2022-07-05 11:04:52 +0100

    Kickoff

---
+++ src/ExampleUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace TestProject.Utils
+{
+    public static class ExampleUtils
+    {
+        /// <summary>
+        /// Get assembly version
+        /// </summary>
+        /// <returns>Assembly version</returns>
+        public static string GetCustomVersion()
+            => "1.1.0";
+    }
+}
---
+++ tests/TestProject.Utils.Tests/ExampleUtilsTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TestProject.Utils.Tests;
+
+public class ExampleUtilsTests
+{
+    [Fact]
+    public void Request_GetAssemblyVersion_AssemblyVersion()
+    {
+        // Arrange && Act
+        var act = ExampleUtils.GetCustomVersion();
+
+
+        // Assert
+        act.Should()
+            .NotBeNullOrWhiteSpace();
+    }
+}
