f0d4d227277178343868ac95a47d04ee025a600a
Nelson Nobre <technobre@gmail.com>
2022-07-05 20:39:11 +0100

    1.3.2

--- src/ExampleUtils.cs
+++ src/ExampleUtils.cs
@@ -7,7 +7,7 @@
         /// </summary>
         /// <returns>Assembly version</returns>
         public static string GetCustomVersion()
-            => "1.3.1";
+            => "1.3.2";
 
         /// <summary>
         /// Get custom framework version
