d0ff6ff7bd8745867e58dc054f8c35956a13ffd3
Nelson Nobre <technobre@gmail.com>
2022-07-05 13:52:13 +0100

    test sourcelink

--- src/ExampleUtils.cs
+++ src/ExampleUtils.cs
@@ -7,7 +7,7 @@
         /// </summary>
         /// <returns>Assembly version</returns>
         public static string GetCustomVersion()
-            => "1.1.1";
+            => "1.1.2";
 
         public static string GetCustomFrameworkVersion()
 #if NET6_0_OR_GREATER
