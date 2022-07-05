68209d1b4efa1a1674c5e90eefaaaf5e31c8afad
Nelson Nobre <technobre@gmail.com>
2022-07-05 14:13:30 +0100

    1.1.5

--- src/ExampleUtils.cs
+++ src/ExampleUtils.cs
@@ -7,7 +7,7 @@
         /// </summary>
         /// <returns>Assembly version</returns>
         public static string GetCustomVersion()
-            => "1.1.4";
+            => "1.1.5";
 
         public static string GetCustomFrameworkVersion()
 #if NET6_0_OR_GREATER
