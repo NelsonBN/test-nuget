26dcaea1cddf91412bef4d612aca1c531638e65f
Nelson Nobre <technobre@gmail.com>
2022-07-07 19:33:39 +0100

    fix: test

--- src/ExampleUtils.cs
+++ src/ExampleUtils.cs
@@ -21,7 +21,7 @@
         /// </summary>
         /// <returns>Assembly version</returns>
         public static string GetCustomVersion()
-            => "1.7.0";
+            => "1.7.1";
 
         /// <summary>
         /// Get custom framework version
